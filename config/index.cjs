c871a751166a1342e5cc9402a483d00f9308835e
Zhengtianxin <80803408+Zhengtianxin@users.noreply.github.com>
2022-10-01 00:02:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '猪猪包',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oDUvL6dKFZZpElOYmlYfGVVNHxw8',
+      id: 'oDUvL6a5wZvbdJniuLDFK_8MUymc',//DUvL6dKFZZpElOYmlYfGVVNHxw8
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'KjClVaAh3HDWIF0Zpf_mAm3sJ5Rv64YS8nwZNX2B_DI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
